f95741fd82a4cc8d93b6e7948ceecedd83b9be86
Andre Felipe <afelipes@gmail.com>
2023-09-25 15:38:18 -0300

    Recuperando QueryParams de rotas

--- src/app/pages/portifolio/card/card.component.ts
+++ src/app/pages/portifolio/card/card.component.ts
@@ -9,13 +9,21 @@ import { ActivatedRoute } from '@angular/router';
 export class CardComponent implements OnInit{
 
   constructor(private activeRoute: ActivatedRoute) { 
+
+    // http://127.0.0.1:4200/portifolio/2
     this.activeRoute.params.subscribe(
+      res => console.log(res)
+    );
+
+    
+    // http://127.0.0.1:4200/portifolio/2?name=maria
+    this.activeRoute.queryParams.subscribe(
       res => console.log(res)
     );
   }
 
-  ngOnInit(): void {
-    
-  }
+
+
+  ngOnInit(): void {}
 
 }
